18db4adadf3f0dbc0c6517189c29f59361ca2876
Vitaliy Taran <vitaliy.t@develops.today>
2020-08-28 23:18:21 +0300

    fix(header): add empty condition

--- src/modules/header/pages/Bag.tsx
+++ src/modules/header/pages/Bag.tsx
@@ -1,5 +1,6 @@
 import * as R from 'ramda';
 import React from 'react';
+import Loader from 'react-loader-spinner';
 import { useSelector, shallowEqual } from 'react-redux';
 import styled from 'styled-components';
 
@@ -7,6 +8,8 @@ import locale from '../../../locale';
 import Container from '../../../components/Container';
 import { getProductFromOrder, getTotalPrice } from '../../products/ProductsReducer';
 import ProductInBagCard from '../components/ProductInBagCard';
+import IsEmpty from '../../../components/IsEmpty';
+import Theme from '../../../components/Theme';
 
 const Bag = () => {
     const order = useSelector(getProductFromOrder, shallowEqual);
@@ -14,15 +17,31 @@ const Bag = () => {
 
     return (
         <>
-            <ContainerWrapper>
-                {!R.isNil(order) &&
-                    !R.isEmpty(order) &&
-                    order.map((item: any) => <ProductInBagCard key={R.prop('id', item)} product={item} />)}
-            </ContainerWrapper>
+            {!R.isNil(order) && !R.isEmpty(order) ? (
+                <ContainerWrapper>
+                    {order.map((item: any) => (
+                        <ProductInBagCard key={R.prop('id', item)} product={item} />
+                    ))}
+                </ContainerWrapper>
+            ) : R.isEmpty(order) ? (
+                <Wrapper>
+                    <IsEmpty>{locale.yourBagIsEmpty}</IsEmpty>
+                </Wrapper>
+            ) : (
+                <Wrapper>
+                    <Loader type="Grid" color={Theme.backgroundsColor.gray} height={80} width={80} />
+                </Wrapper>
+            )}
             {!R.isNil(order) && !R.isEmpty(order) && <TotalPrice>{`${locale.totalPrice} ${totalPrice}`}</TotalPrice>}
         </>
     );
 };
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 const ContainerWrapper = styled(Container)`
     display: flex;
