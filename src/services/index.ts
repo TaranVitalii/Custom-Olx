29275a807f4a601b879cf3dbfebc631089add580
Vitaliy Taran <vitaliy.t@develops.today>
2020-09-03 10:59:18 +0300

    feat(services): extend services object

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,10 +1,23 @@
+import axios from 'axios';
+import * as R from 'ramda';
+
 import { URLS } from '../models';
+import { pageProps } from '../interfaces';
+import makeStringFromArray from '../helpers';
 
-export const fetchProductsRequest = async () => {
+export const fetchProductsRequest = async ({ page = 1, origins, maxPrice, minPrice }: pageProps) => {
     try {
-        const response = await fetch(URLS.FETCH_PRODUCTS);
+        const response = await axios(URLS.FETCH_PRODUCTS, {
+            params: {
+                page,
+                origins: makeStringFromArray(origins),
+                maxPrice,
+                minPrice,
+            },
+        });
+        const data = R.prop('data', response);
 
-        return response.json();
+        return data;
     } catch (e) {
         return null;
     }
@@ -12,9 +25,21 @@ export const fetchProductsRequest = async () => {
 
 export const fetchProductByIdRequest = async (id: any) => {
     try {
-        const response = await fetch(URLS.FETCH_PRODUCT_BY_ID + id);
+        const response = await axios(URLS.FETCH_PRODUCT_BY_ID + id);
+        const data = R.prop('data', response);
 
-        return response.json();
+        return data;
+    } catch (e) {
+        return null;
+    }
+};
+
+export const fetchProductOriginsRequest = async () => {
+    try {
+        const response = await axios(URLS.FETCH_PRODUCTS_ORIGINS);
+        const data = R.prop('data', response);
+
+        return data;
     } catch (e) {
         return null;
     }
