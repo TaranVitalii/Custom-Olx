78bbff683bd0487bc29086cc2acd29207df67b55
Vitaliy Taran <vitaliy.t@develops.today>
2020-09-17 02:27:40 +0300

    feat(store): add new saga

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,14 +1,15 @@
 import createSagaMiddleware from 'redux-saga';
 import { fork, all } from 'redux-saga/effects';
 
-import ProductsSaga from '../modules/products/ProductsSaga';
+import ProductsSaga from 'modules/products/ProductsSaga';
+import HeaderSaga from 'modules/header/HeaderSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default sagaMiddleware;
 
 function* root() {
-    yield all([fork(ProductsSaga)]);
+    yield all([fork(ProductsSaga), fork(HeaderSaga)]);
 }
 
 export const run = () => sagaMiddleware.run(root);
