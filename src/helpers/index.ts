460ad15519efb1018be9cff9b7ae657867a5c7fd
Vitaliy Taran <vitaliy.t@develops.today>
2020-09-17 02:23:49 +0300

    fix(helpers): refactor import/export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,1 @@
-import * as R from 'ramda';
-
-const makeStringFromArray = (origins: string[] | null): string | null => {
-    if (R.isNil(origins) || R.isEmpty(origins)) return null;
-
-    return origins.join(',');
-};
-
-export default makeStringFromArray;
+export { default as makeStringFromArray } from './makeStringFromArray';
